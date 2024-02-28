7d90c60ce12d595d53f5a5bd7f1ca59211fbc86f
Pawan Bafila <123593267+Pawanbafila@users.noreply.github.com>
2024-02-29 02:17:19 +0530

    we add some more pages

---
+++ script.js
@@ -0,0 +1,60 @@
+// DOM Element's
+const counters = document.querySelectorAll('.counter');
+
+/*** Using forEach() ***/
+
+/*
+
+counters.forEach((counter) => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+        const divider = 5000;
+        const speed = 1000;
+
+        const inc = target / divider;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, speed);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
+
+*/
+
+/*** Same functionality, now using for...of ***/
+
+for (let n of counters) {
+    const updateCount = () => {
+        const target = +n.getAttribute('data-target');
+        const count = +n.innerText;
+        const divider = 5000;
+        const speed = 10; // 1000 millisecond => 1 second;
+
+        const inc = target / divider;
+
+        if (count < target) {
+            n.innerText = Math.ceil(count + inc);
+            setTimeout(updateCount, speed);
+        } else {
+            n.innerText = target;
+        }
+    };
+
+    updateCount();
+}
+
+
+gsap.from( ".ser-ban-left", {
+    z:-500,
+    duration: 1 ,
+    opacity:0,
+    delay:1,
+   
+})
+
